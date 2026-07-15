01816c2f96fe106b038c747ee39f4f01b58718c2
CitrusAde <93243276+CitrusAde@users.noreply.github.com>
2026-07-15 11:05:40 +0900

    Update stages.js

--- stages.js
+++ stages.js
@@ -16,7 +16,7 @@ const defaultMaps = [
         name: "스테이지 2: 첫 번째 탐험",
         desc: "배경(bg_1.jpg) 위에 지형(map_1.png)을 얹은 맵입니다.",
         isDefault: false,
-        bgStyle: "url('bg_1.jpg') center/cover", // 도구리가 통과하는 예쁜 배경
+        bgStyle: "url('bg_1.png') center/cover", // 도구리가 통과하는 예쁜 배경
         terrainData: "map_1.png",               // 도구리가 밟고 파괴할 투명 지형
         spawns: [{x: 100, y: 100}],   
         exits: [{x: 850, y: 450}]
@@ -26,7 +26,7 @@ const defaultMaps = [
         name: "스테이지 3: 험난한 여정",
         desc: "배경(bg_2.jpg) 위에 지형(map_2.png)을 얹은 맵입니다.",
         isDefault: false,
-        bgStyle: "url('bg_2.jpg') center/cover",
+        bgStyle: "url('bg_2.png') center/cover",
         terrainData: "map_2.png",
         spawns: [{x: 50, y: 50}],   
         exits: [{x: 900, y: 500}]
